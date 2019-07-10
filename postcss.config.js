e586b9cfa2f12494eef59ca20f83cfcfca83a869
MikeCheng1208 <a3804430@hotmail.com>
2019-07-11 00:10:27 +0800

    setting fix

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-    return {
-        plugins: {
-            "autoprefixer": {
-                browsers: ['> 1%', 'last 5 versions', 'Firefox >= 45', 'iOS >=8', 'Safari >=8','ie >= 10']
-            },
-        }
-    };
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const webpack = require('webpack');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const config = {
     context: path.resolve(__dirname, 'src'),
